df09ae002aafe50dd8c9de85d9c601a3d5a045d1
Jackson Tsai <jacksontsai@Jackson.local>
2024-09-05 22:19:22 -0600

    chore: install react-hook, zod, chartjs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'warm-pink': '#FB7474',
+        'navy-blue': '#16538E'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
